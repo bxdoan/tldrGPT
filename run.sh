4c8c9330041f6a86b3eed22c3c640b057c176021
Doan Bui <bxdoan93@gmail.com>
2023-06-01 14:32:05 +0700

    tldr

--- run.sh
+++ run.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
+s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME="$s"  # get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
 
 # set pipenv path
-if [ `uname` = "Darwin" ]; then
-    pipenv="/Users/`whoami`/.local/bin/pipenv"
+if [[ -f "$HOME/.pyenv/shims/pipenv" ]]; then
+  pipenv="$HOME/.pyenv/shims/pipenv"
+elif [[ -f "$HOME/.local/bin/pipenv" ]]; then
+  pipenv="$HOME/.local/bin/pipenv"
+elif [[ -f "/opt/homebrew/bin/pipenv" ]]; then
+  pipenv="/opt/homebrew/bin/pipenv"
+elif [[ -f "/usr/local/bin/pipenv" ]]; then
+  pipenv="/usr/local/bin/pipenv"
 else
-    pipenv="/home/`whoami`/.pyenv/shims/pipenv"
+  echo "pipenv application not found"
 fi
 
 PYTHONPATH=`pwd` $pipenv run python tldrGPT.py -url $@
+
+if [[ -z "$pipenv" ]]; then
+  echo "pipenv not found, run python directly"
+  PYTHONPATH=`pwd` python3 "$SCRIPT_HOME/tldrGPT.py" -url "$@"
+else
+  PYTHONPATH=`pwd` $pipenv run python3 "$SCRIPT_HOME/tldrGPT.py" -url "$@"
+fi
